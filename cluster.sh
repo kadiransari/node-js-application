3120d46dfcf94d19f4b1e3a82730b4b411720a36
root <root@ip-172-31-7-123.ap-south-1.compute.internal>
2024-10-17 15:53:28 +0000

    upload code for node js application

---
+++ cluster.sh
@@ -0,0 +1,35 @@
+#aws-cli-install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#install-kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.4/2023-08-16/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.4/2023-08-16/bin/linux/amd64/kubectl.sha256
+sha256sum -c kubectl.sha256
+openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+kubectl version --short --client
+
+
+#install-eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin/
+eksctl version
+
+#add access key and secret key
+aws configure
+export AWS_ACCESS_KEY_ID=AKIAZOZQF5THZHMIT5PQ
+export AWS_SECRET_ACCESS_KEY=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
+
+#create-cluster
+eksctl create cluster --name kadir-cluster --version 1.30 --region ap-south-1 --node-type t3.medium --nodes 2 --nodes-min 1 --nodes-max 4 --managed
+
+
+
+#helm-install
+ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+ chmod 700 get_helm.sh
+ ./get_helm.sh
